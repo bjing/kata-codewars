701cacab1b875d9287e3483b24155fb9ce0c7445
bjing <brian.jing@myob.com>
2016-06-06 10:19:51 +1000

    Solution for combinations

---
+++ haskell/6kyu/combinations.hs
@@ -0,0 +1,7 @@
+module Combinations where
+import Data.List
+
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _  = [ [] ]
+combinations n xs = [ y:ys | y:xs' <- tails xs
+                           , ys <- combinations (n-1) xs']
