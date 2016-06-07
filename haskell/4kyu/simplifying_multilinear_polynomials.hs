970ac2a40e336674827998b582e28909fca45ac5
bjing <brian.jing@myob.com>
2016-06-07 16:44:45 +1000

    simply multilinear polynomials WIP

--- haskell/4kyu/simplifying_multilinear_polynomials.hs
+++ haskell/4kyu/simplifying_multilinear_polynomials.hs
@@ -1,4 +1,18 @@
 module Codewars.Kirilloid.MultilinearPolynomials where
+import Data.List
+import Data.List.Split
 
 simplify :: String -> String
-simplify = undefined
+simplify = reduceIt . splitIt
+
+splitIt :: String -> [String]
+splitIt = split (keepDelimsL $ oneOf "+-")
+
+--dedupIt :: [String] -> [String]
+
+-- TODO need to group different elements
+-- within a group, do the following:
+-- takeWhile (not . isAlpha) "+2abc"
+
+reduceIt :: [String] -> String
+reduceIt = (foldl (++) "") . (map sort)
