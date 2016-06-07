75a272ae320bc84a26387beccd3bbeaf91843179
bjing <brian.jing@myob.com>
2016-06-07 11:14:51 +1000

    Add a smarter alternate solution to the Combinations kata

--- haskell/6kyu/combinations.hs
+++ haskell/6kyu/combinations.hs
@@ -1,7 +1,16 @@
 module Combinations where
 import Data.List
+import Control.Monad
 
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _  = [ [] ]
 combinations n xs = [ y:ys | y:xs' <- tails xs
                            , ys <- combinations (n-1) xs']
+
+
+-- Another more elegant solution
+combinations' :: Int -> [a] -> [[a]]
+combinations' n = filter ((==n) . length) . sublists
+
+sublists :: [a] -> [[a]]
+sublists = filterM (const [True, False])
