fac129b3ca855f6678158a58ebf25727e923c00a
Brian Jing <brianjing404@gmail.com>
2016-06-07 22:21:01 +1000

    simplify multilinear polynomials WIP

--- haskell/4kyu/simplifying_multilinear_polynomials.hs
+++ haskell/4kyu/simplifying_multilinear_polynomials.hs
@@ -3,44 +3,84 @@ import Data.List
 import Data.List.Split
 import Data.Char
 import GHC.Exts
+import Data.Ord (comparing)
 
 simplify :: String -> String
--- TODO compose more functions in the pipeline
-simplify = reduceIt . transformIt . splitIt
+simplify = combineIt . transformIt . splitIt
 
+-- Split polynomials
 splitIt :: String -> [String]
-splitIt = split (keepDelimsL $ oneOf "+-")
+splitIt = (map sort) . (filter (\e -> length e /= 0)) . split (keepDelimsL $ oneOf "+-")
 
-reduceIt :: [String] -> String
-reduceIt = (foldl (++) "") . (map sort)
+-- Combine transformed split polynomials terms back together
+combineIt :: [String] -> String
+combineIt = removeLeadingPlus . (foldl (++) "")
 
---dedupIt :: [String] -> [String]
+-- Add up coefficients for elements with the same indeterminates
+transformIt = removeZeroTerms . reconTerms . tupleSort . sumCoeffs . separateCoeffs
 
+----------------------
+-- Helper functions
+----------------------
+-- Remove leading + sign for processed polynomial string
+removeLeadingPlus s
+  | isPrefixOf "+" s = tail s
+  | otherwise = s
 
+-- Separate coefficient from each term and store coeff and indeterminate in a tuple
+-- Also group all elements that have the same indeterinates
+separateCoeffs = (map unzip . groupWith (\(c, n) -> n)) . (map separateCoeffForTerm)
+-- Separate coefficient from indeterinate for a single term
+separateCoeffForTerm s = ((normaliseCoeffPre . extractCoeff) s, extractBase s)
+-- Sum coefficients for each group
+sumCoeffs = map (\(c, n) -> (sum(map stringToInt c), (head n)))
+-- Combine coeffs and indeterminates
+reconTerms = map (\(i, n) -> normaliseCoeffPost (intToString i ++ n))
+-- Remove terms with 0 coefficients
+removeZeroTerms = filter (\t -> not $ isPrefixOf "+0" t)
+
+-- Extract coefficient from an element
 extractCoeff = takeWhile (not . isAlpha)
-extractNumber = dropWhile (not . isAlpha)
-normaliseCoeff :: [Char] -> [Char]
-normaliseCoeff c
+-- Extract indeterminates from an element
+extractBase = dropWhile (not . isAlpha)
+
+-- Normalise coefficients for processing
+normaliseCoeffPre :: [Char] -> [Char]
+normaliseCoeffPre c
   | c == "-" = "-1"
-  | c == "+" = "1"
+  | c == "+" || c == "" = "1"
   | isPrefixOf "+" c && length c > 1 = drop 1 c
   | otherwise = c
-separateCoefficients s = ((normaliseCoeff . extractCoeff) s, extractNumber s)
+-- Normalise strings after processing
+normaliseCoeffPost s
+--  | isPrefixOf "-1" s = "-" ++ (drop 2 s)
+  | (extractCoeff s) == "-1" = "-" ++ (extractBase s)
+--  | isPrefixOf "1" s = "+" ++ (tail s)
+  | (extractCoeff s) == "1" = "+" ++ (extractBase s)
+  | not $ isPrefixOf "-" s = "+" ++ s
+  | otherwise = s
 
+-- Sort list of tuples of (String, String) given length of the second string
+tupleSort = sortBy (comparing $ length . snd)
 
+-- String and Int conversion
 intToString :: Int -> String
 intToString i = show i
 stringToInt :: String -> Int
-stringToInt s = read s :: Int
+stringToInt s = (read s) :: Int
 
-transformIt = normaliseAll . sumCoeffs . separateAllCoeffs
-separateAllCoeffs = (map unzip . groupWith (\(c, n) -> n)) . (map separateCoefficients)
-sumCoeffs = map (\(c, n) -> (sum(map stringToInt c), (head n)))
-normaliseAll = map (\(i, n) -> normalise (intToString i ++ n))
-normalise s
-  | isPrefixOf "-1" s = "-" ++ (drop 2 s)
-  | isPrefixOf "1" s = "+" ++ (tail s)
-  | otherwise = s
+----------------
+-- Sample input
+----------------
+sample = "2abc-bc-abc"
+sample1 = "dc+dcba"
+sample2 = "2xy-yx"
+sample3 = "-a+5ab+3a-c-2a"
+sample4 = "-abc+3a+2ac"
+sample5 = "xyz-xz"
+sample6 = "-abc+3a+2ac"
+sample7 = "4y-8dy+14dyz-24dyz"
+
 
 -- Notes
 -- splitRes = ["+2abc", "-bc", "-abc"]
