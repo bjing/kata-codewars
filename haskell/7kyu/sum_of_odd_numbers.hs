d0d9dd196e77a66c10b9516b0b6aa8a5c9be63c4
bjing <brian.jing@myob.com>
2016-06-08 11:52:15 +1000

    Add solution to summing odd numbers

--- haskell/7kyu/sum_of_odd_numbers.hs
+++ haskell/7kyu/sum_of_odd_numbers.hs
@@ -1,10 +1,4 @@
 module Codewars.SumOddNumbers where
 
 rowSumOddNumbers :: Integer -> Integer
-rowSumOddNumbers n = sum[firstColumn n, firstColumn n + 2..lastColumn n]
-
-firstColumn :: Integer -> Integer
-firstColumn n = n * (n - 1) + 1
-
-lastColumn :: Integer -> Integer
-lastColumn n = firstColumn n + (n - 1) * 2
+rowSumOddNumbers n = n * n * n
