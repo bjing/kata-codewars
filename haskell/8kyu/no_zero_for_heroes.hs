f3ad3d3f37dd986d58e51fd2a51d6bde18f8285d
Brian Jing <brianjing404@gmail.com>
2016-07-19 22:05:10 +1000

    Add final solutoin for No Zero for Heroes

--- haskell/8kyu/no_zero_for_heroes.hs
+++ haskell/8kyu/no_zero_for_heroes.hs
@@ -3,6 +3,5 @@ module Codewars.G964.NoZeros where
 noBoringZeros :: Int -> Int
 noBoringZeros n
   | n == 0 = 0
-  | divisibleByTen n = noBoringZeros (n `div` 10)
+  | n `mod` 10 == 0 = noBoringZeros $ n `div` 10
   | otherwise = n
-  where divisibleByTen n = if n `mod` 10 == 0 then True else False
