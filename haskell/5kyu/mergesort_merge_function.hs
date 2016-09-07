fe8f5e5f8f6fd1f67840f11c896dd23b5972cbfa
Brian Jing <brian.jing@myob.com>
2016-09-07 13:02:02 +1000

    mergesort merge function WIP

--- haskell/5kyu/mergesort_merge_function.hs
+++ haskell/5kyu/mergesort_merge_function.hs
@@ -4,13 +4,12 @@ merge :: (Ord a, Num a) => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
 merge xs ys
-  | sum xs < sum ys = xs `mappend` ys
-  | otherwise = ys `mappend` xs
+  | comp xs ys = xs ++ ys
+  | otherwise = ys ++ xs
 
 comp :: Ord a => [a] -> [a] -> Bool
-comp [] [] = True
 comp [] _ = True
-comp _ [] = False
+comp _ [] = True
 comp (x:xs) (y:ys)
-  | x < y = comp xs ys
+  | x <= y = comp xs ys
   | otherwise = False
