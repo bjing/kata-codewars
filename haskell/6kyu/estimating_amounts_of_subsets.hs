f017b03a38eb001004a05bac7a0b2c76fd30bb43
Brian Jing <brian.jing@myob.com>
2017-01-25 16:44:44 +1100

    Add solution(s) to Estimating Amounts of Subsets

--- haskell/6kyu/estimating_amounts_of_subsets.hs
+++ haskell/6kyu/estimating_amounts_of_subsets.hs
@@ -1,12 +1,25 @@
 module Kata (estSubsets) where
 
 import Data.List
+import Control.Monad
 
 estSubsets :: Ord a => [a] -> Integer
-estSubsets = toInteger . length . sublists
+estSubsets l = 2^(length $ dedup l) - 1
 
-sublists :: Ord a => [a] -> [[a]]
-sublists l = tail . dedup . concatMap inits $ tails l
+dedup :: Ord a => [a] -> [a]
+dedup = fmap head . group . sort
 
-dedup :: Ord a => [[a]] -> [[a]]
-dedup = fmap head . group. sort
+----------------------------------------------------------------------
+
+-- This answer is too slow to get a pass from Codewars submit tool
+estSubsets' :: Ord a => [a] -> Integer
+estSubsets' = toInteger . length . removeBlank . sublists . dedup
+
+sublists :: [a] -> [[a]]
+sublists = filterM (const [True, False])
+
+dedup :: Ord a => [a] -> [a]
+dedup = fmap head . group . sort
+
+removeBlank :: Ord a => [[a]] -> [[a]]
+removeBlank = tail . sort
